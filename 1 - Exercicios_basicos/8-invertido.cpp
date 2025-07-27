acf8e2227eef51a023e0aca29ff2250ceb2c67e6
hamiltonvr <hamiltonventtura@gmail.com>
2025-07-27 13:33:55 -0300

    Atualização 7 e 8

--- 1 - Exercicios_basicos/8-invertido.cpp
+++ 1 - Exercicios_basicos/8-invertido.cpp
@@ -1,16 +1,19 @@
-#include <iostream.h>
-main()
-{
-       char A[20], B[20];
-       int i = 0, n, j;
+#include <iostream>
+#include <string>
+using namespace std;
 
-       gets(A);
-       n = strlen(A);
-       for (j = n - 1; i < n; i++)
-       {
-              j--;
-              A[i] = B[j];
-       }
-       puts(A);
-       system("pause");
+int main() {
+    string A;
+    cout << "Digite uma palavra ou frase: ";
+    getline(cin, A); // segura e moderna
+
+    string B = A; // cópia para inverter
+
+    int n = A.length();
+    for (int i = 0, j = n - 1; i < j; i++, j--) {
+        swap(B[i], B[j]); // troca os caracteres
+    }
+
+    cout << "Invertida: " << B << endl;
+    return 0;
 }
